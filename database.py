1f08095036243c5854f06847b609ad5173dcb668
Mmeso12-ma <ezeobimmesoma66@gmail.com>
2025-09-03 16:45:46 +0100

    corrected an error

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import settings
-DATABASR_URL = settings.DATABASE_URL
-engine = create_engine(DATABASR_URL)
+engine = create_engine(settings.DATABASR_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
